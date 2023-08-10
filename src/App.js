ca26a545f3285c2d6bea212284b423e6ba1f3ee2
Akshay Patel <ap7839426@gmail.com>
2023-08-11 02:05:38 +0530

    resolved non rendering and add custom button component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import HomePage from './pages/homepage/homepage.components.jsx';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+
 import { Route, Routes } from 'react-router-dom';
 
 function App() {
@@ -11,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" Component={HomePage} />
         <Route path="/shop" Component={ShopPage} />
+        <Route path='/signin' Component={SignInAndSignUpPage}/>
       </Routes>
     </div>
   );
